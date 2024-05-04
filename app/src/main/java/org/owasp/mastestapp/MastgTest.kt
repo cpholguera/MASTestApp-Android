7a71412b2857d4616c61b7c85386b297de9f4091
Carlos Holguera <perezholguera@gmail.com>
2024-05-04 10:55:40 +0200

    update base test to return string value

--- app/src/main/java/org/owasp/mastestapp/MastgTest.kt
+++ app/src/main/java/org/owasp/mastestapp/MastgTest.kt
@@ -3,11 +3,13 @@ package org.owasp.mastestapp
 import android.util.Log
 import android.content.Context
 
-
 class MastgTest (private val context: Context){
 
-    fun mastgTest0000() {
-        Log.d("SampleCode", "Hello from the OWASP MASTG Test app.")
+    fun mastgTest(): String {
+        val sensitiveString = "Hello from the OWASP MASTG Test app."
+
+        Log.d("MASTG-TEST", sensitiveString)
+        return sensitiveString
     }
 
 }
