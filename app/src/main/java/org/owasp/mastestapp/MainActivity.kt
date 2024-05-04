c2bba4ab600f8f14a03f593436e40ba006b75351
Carlos Holguera <perezholguera@gmail.com>
2024-05-04 10:31:07 +0200

    simplified to one button

--- app/src/main/java/org/owasp/mastestapp/MainActivity.kt
+++ app/src/main/java/org/owasp/mastestapp/MainActivity.kt
@@ -11,12 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import org.owasp.mastestapp.MastgTest
 import org.owasp.mastestapp.ui.theme.MASTestAppTheme
@@ -35,25 +40,28 @@ class MainActivity : ComponentActivity() {
 fun MyScreenContent() {
     val context = LocalContext.current
     val mastgTestClass = MastgTest(context)
-    val mastgTestFunctions = MastgTest::class.members.filter {
-        it.name.startsWith("mastgTest")
-    }
+    var displayString by remember { mutableStateOf("OWASP MAS") }
+
 
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        mastgTestFunctions.forEach { function ->
-            Button(
-                onClick = {
-                    function.call(mastgTestClass)
-                },
-                modifier = Modifier.padding(16.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
-            ) {
-                Text(text = function.name)
-            }
+        // Create a text view
+        Text(
+            text = displayString,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = {
+                displayString = mastgTestClass.mastgTest()
+            },
+            modifier = Modifier.padding(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+        ) {
+            Text(text = "Start")
         }
+
     }
 }
