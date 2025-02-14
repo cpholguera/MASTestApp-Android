2ee5be84c8970bbb76a48b2066287419d7630da8
Carlos Holguera <perezholguera@gmail.com>
2025-02-14 19:21:18 +0100

    refactor extract-code-for-mastg-demo.sh to streamline APK handling and output file management

--- tools/extract-code-for-mastg-demo.sh
+++ tools/extract-code-for-mastg-demo.sh
@@ -4,27 +4,22 @@ CLASS_NAME="org.owasp.mastestapp.MastgTest"
 OUTPUT_DIR="output"
 TEMP_APKTOOL_DIR="$OUTPUT_DIR/temp_apktool_output"
 TEMP_APK="$OUTPUT_DIR/temp_base.apk"
-APK_PATH="./app/build/outputs/apk/debug/app-debug.apk"
+ANDROID_MANIFEST_PATH="./app/src/main/AndroidManifest.xml"
+MASTG_TEST_PATH="./app/src/main/java/org/owasp/mastestapp/MastgTest.kt"
 
 # Create a temporary directory for jadx output
 TEMP_JADX_OUTPUT_DIR="$OUTPUT_DIR/temp_jadx_output"
-
-# Build Android Studio project
-gradle assembleDebug
 
 # Create temporary directories for output
 mkdir -p "$OUTPUT_DIR"
 mkdir -p "$TEMP_APKTOOL_DIR"
 mkdir -p "$TEMP_JADX_OUTPUT_DIR"
 
-# Copy the APK to the temporary location
-cp "$APK_PATH" "$TEMP_APK"
-
 # Get the APK path from the device
-# APK_PATH=$(adb shell pm path org.owasp.mastestapp | sed 's/package://')
+APK_PATH=$(adb shell pm path org.owasp.mastestapp | sed 's/package://')
 
 # Pull the APK to a temporary location
-# adb pull "$APK_PATH" "$TEMP_APK"
+adb pull "$APK_PATH" "$TEMP_APK"
 
 # Use apktool to extract the AndroidManifest.xml
 apktool d -s -f -o "$TEMP_APKTOOL_DIR" "$TEMP_APK"
@@ -51,9 +46,11 @@ else
 fi
 
 # Copy the original AndroidManifest.xml and MastgTest.kt to the output directory
+cp "$ANDROID_MANIFEST_PATH" "$OUTPUT_DIR"
+cp "$MASTG_TEST_PATH" "$OUTPUT_DIR"
 
-cp app/src/main/AndroidManifest.xml "$OUTPUT_DIR/AndroidManifest.xml"
-cp app/src/main/java/org/owasp/mastestapp/MastgTest.kt "$OUTPUT_DIR/MastgTest.kt"
+# Move the APK to the output directory
+mv "$TEMP_APK" "$OUTPUT_DIR"
 
 # Clean up temporary files
 rm -rf "$TEMP_APK" "$TEMP_JADX_OUTPUT_DIR" "$TEMP_APKTOOL_DIR"
